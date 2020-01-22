360b08288314a228922728ff156bbfe41dc7f6b1
suyashpatni <44916178+suyashpatni@users.noreply.github.com>
2020-01-22 12:32:42 +0800

    Update fourierTransform.py

--- fourierTransform.py
+++ fourierTransform.py
@@ -10,7 +10,7 @@ import path
 pygame.init()
 
 drawing = path.drawing
-itera = len(drawing)//2
+itera = 250#len(drawing)//2
 height = 900
 width = 1500
 screen = pygame.display.set_mode((width, height))
@@ -18,14 +18,16 @@ clock = pygame.time.Clock()
 done = False
 endpointsList = []
 vectors_list = []
-
+font = pygame.font.Font('freesansbold.ttf', 50) 
 RED = pygame.Color("red")
 BLUE = pygame.Color('blue')
 WHITE = pygame.Color('white')
 BLACK = pygame.Color('black')
 GREY = pygame.Color(30,30,30)
 font = pygame.font.Font('freesansbold.ttf', 50) 
-
+text = font.render('Fourier Series', True, WHITE, BLACK) 
+textRect = text.get_rect()  
+textRect.center = (750, 50)   
 speed = 1                                                                                 
 for i in range(itera):   
     ang = 180*fourierCalc.fourier(drawing, i)[1]/math.pi
@@ -53,7 +55,7 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
                
-        
+    screen.blit(text, textRect)  
     for i in range(itera*2-1):
         
         n = vectors_list[i].speed/speed
@@ -83,6 +85,7 @@ while not done:
         except Exception as e:
             pass
     if len(endpointsList) >= 360/speed:
+        print(endpointsList)
         endpointsList = []
 
     
