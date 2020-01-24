99785a5fda45445a4a78d0b87c5af7c00358b38c
suyashpatni <44916178+suyashpatni@users.noreply.github.com>
2020-01-24 15:33:29 +0800

    You can now view the fps at the top left corner

--- fourierTransform.py
+++ fourierTransform.py
@@ -6,13 +6,12 @@ import random
 import fourierCalc
 import path
 
-
 pygame.init()
 
 drawing = path.drawing
 itera = len(drawing)//2
-height = 900
-width = 1500
+height = 800
+width = 1000
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 done = False
@@ -20,14 +19,15 @@ endpointsList = []
 vectors_list = []
 font = pygame.font.Font('freesansbold.ttf', 50) 
 RED = pygame.Color("red")
-BLUE = pygame.Color('blue')
 WHITE = pygame.Color('white')
 BLACK = pygame.Color('black')
 GREY = pygame.Color(30,30,30)
+
 font = pygame.font.Font('freesansbold.ttf', 50) 
+font_fps = pygame.font.Font('freesansbold.ttf', 15) 
 text = font.render('Fourier Series', True, WHITE, BLACK) 
 textRect = text.get_rect()  
-textRect.center = (750, 50)   
+textRect.center = (width/2, 50)   
 speed = 1                                                                                 
 for i in range(itera):   
     ang = 180*fourierCalc.fourier(drawing, i)[1]/math.pi
@@ -54,7 +54,10 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-               
+    text_fps = font_fps.render('fps: {}'.format(str(round(clock.get_fps(), 3))), True, WHITE, BLACK)  
+    textRect_fps = text_fps.get_rect()  
+    textRect_fps.center = (width/8, 50)   
+    screen.blit(text_fps, textRect_fps)
     screen.blit(text, textRect)  
     for i in range(itera*2-1):
         
@@ -84,12 +87,12 @@ while not done:
 
         except Exception as e:
             pass
-    if len(endpointsList) >= 360/speed:
-        print(endpointsList)
+    if len(endpointsList) >= 360/speed:       
         endpointsList = []
-
-    
+   
     pygame.display.flip()
-    clock.tick(0)
 
+    clock.tick(0)
+    
+    
 pygame.quit()
