d5bdf08178c79489624902c39b1627c2f6e1893f
suyashpatni <44916178+suyashpatni@users.noreply.github.com>
2020-05-31 18:25:08 +0800

    Update fourierCalc.py

--- fourierCalc.py
+++ fourierCalc.py
@@ -8,8 +8,7 @@ def fourier(func, i):
         re_func = func[k]['x']-200
         im_func = func[k]['y']-200
         phi = (2*math.pi*i*k)/N
-        fun += (re_func + im_func*1j)*(math.cos(phi) - math.sin(phi)*1j)
-    fun = fun/N
+        fun += ((re_func + im_func*1j)*(math.e**(phi*-1j)))/N
     angle = math.atan2(fun.imag,fun.real)
     length = math.sqrt(fun.imag**(2)+fun.real**(2))
     return [length, angle]
